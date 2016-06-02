06b855e2e7ae08d14072647fd0f9ce48d15d16ae
Mark Lutze <mlutze@gmail.com>
2016-06-01 21:19:10 -0600

    initial commit

---
+++ LungCapPlot.R
@@ -0,0 +1,15 @@
+rm(list=ls())
+
+LungCapData <- read.delim("~/RProgrammingCourse/LungCapData.txt")
+
+attach(LungCapData)
+
+count <- table(Gender)
+
+pct <- count/725
+
+out.thing <-  barplot(pct,
+        main = 'Percent Male vs Female ' , 
+        xlab="Gender", ylab="%", las=TRUE, 
+        names.arg = c("Chicks","Dudes"), 
+        col = c("RED","GREEN") ) 
